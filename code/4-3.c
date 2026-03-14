695a246f645d6d61b6a1c5a3f0a4c7e132ba9b4a
goddns777 <152259531+goddns777@users.noreply.github.com>
2026-03-14 21:42:57 +0900

    Update 4-3.c

--- code/4-3.c
+++ code/4-3.c
@@ -2,7 +2,30 @@
 
 int main()
 {
+    int a, x;
+    int result;
+
+    result = 1;
+    
+    scanf("%d", &a);
+
+    if (a <= 1){
+        result = 0;
+    }else{
+        for(x = 2; x < a; x++){
+            if(a % x ==0){
+                result = 0;
+                break;
+            }
+        }
+    }
+    
+    if (result == 0){
+        printf("false");
+    }
+    if (result == 1){
+        printf("true");
+    }
 
     return 0;
 }
-
