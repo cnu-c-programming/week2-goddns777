5eeb4715211ace4d365d942f08dd391b92743f85
goddns777 <152259531+goddns777@users.noreply.github.com>
2026-03-14 20:56:37 +0900

    Update 4-1.c

--- code/4-1.c
+++ code/4-1.c
@@ -2,6 +2,28 @@
 
 int main()
 {
+    int a,b;
+    char x;
+    int result;
+
+    scanf("%d %d %c", &a, &b, &x);
+    
+    if (x == '+') {
+        result = a + b;
+        printf("%d",result);
+    }else if (x =='-')
+    {
+        result = a - b;
+        printf("%d",result);
+    }else if (x == '*')
+    {
+        result = a * b;
+        printf("%d",result);
+    }else if (x == '/')
+    {
+        result = a / b;
+        printf("%d",result);
+    }
 
     return 0;
 }
